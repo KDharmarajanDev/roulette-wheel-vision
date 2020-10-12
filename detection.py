73c463494b24256bd7995165813584a13cb35d74
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2020-10-11 22:05:25 -0700

    Added Debug Flags

--- detection.py
+++ detection.py
@@ -1,4 +1,5 @@
 import numpy as np
+import argparse
 import cv2 as cv
 import heapq
 import matplotlib as mpl
@@ -40,10 +41,15 @@ def heuristic(contour):
     area = rect[1][0] * rect[1][1]
     diff = cv.contourArea(cv.convexHull(contour)) - cv.contourArea(contour)
     cent = rect[0]
-    heur = 3 * area - 11 * diff
+    heur = 3 * area - 9 * diff
     return heur
 
-cap = cv.VideoCapture('/Users/karthikdharmarajan/Documents/URobotics/Course Footage/GOPR1146.MP4')
+parser = argparse.ArgumentParser(description='Roulette Wheel Vision')
+parser.add_argument('-hist', action='store_true', help='Outputs a histogram every n frames specified by -n', default=False)
+parser.add_argument('-n', help='The amount of frames after which a histogram is displayed', type=int, default=100)
+parser.add_argument('-equalized', action='store_true', help='This flag sets if the histogram displayed is after image histogram equalization', default=False)
+args = parser.parse_args()
+cap = cv.VideoCapture('/Users/karthikdharmarajan/Documents/URobotics/Course Footage/GOPR1145.MP4')
 saliency = cv.saliency.StaticSaliencySpectralResidual_create()
 count = 0
 
@@ -63,10 +69,8 @@ while cap.isOpened():
             
             # Dilation to connect the wheel more
             dilated = cv.dilate(threshold, cv.getStructuringElement(cv.MORPH_ELLIPSE, (30,30)))
-            # cv.imshow("dilated", dilated)
 
-             # Getting Contours
-             # Plot Histograms Across the Duration of the Video (and see how they fluctuate)
+            # Getting Contours
             contours, _ = cv.findContours(dilated,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
             if len(contours) > 0:
                 largest_countour = max(contours,key=cv.contourArea)
@@ -74,8 +78,8 @@ while cap.isOpened():
                 spinner = img_in[y:y+h, x:x+w]
 
                 hsv_spinner = cv.cvtColor(spinner,cv.COLOR_BGR2HSV)
-                blurred_spinner = cv.medianBlur(hsv_spinner,7)
-                # cv.imshow('blurred_spinner', cv.cvtColor(blurred_spinner,cv.COLOR_HSV2BGR))
+                blurred_spinner = cv.bilateralFilter(hsv_spinner,7,50,50)
+                cv.imshow('blurred_spinner', cv.cvtColor(blurred_spinner,cv.COLOR_HSV2BGR))
 
                 # Index 0: Hue, Index 1: Saturation, Index 2: Value
                 individual_channels = cv.split(blurred_spinner)
@@ -85,41 +89,35 @@ while cap.isOpened():
                 individual_channels[2] = cv.equalizeHist(individual_channels[2])
 
                 equalized_image = cv.merge((individual_channels[0],individual_channels[1],individual_channels[2]))
-                # cv.imshow('equalized_image', cv.cvtColor(equalized_image, cv.COLOR_HSV2BGR))
-                # if count % 100 == 0:
-                #     draw_color_histogram(equalized_image,colors=['c','y','m'],color_labels=['Hue','Saturation','Value'])
-                # count += 1
+                cv.imshow('equalized_image', cv.cvtColor(equalized_image,cv.COLOR_HSV2BGR))
+
+                if args.hist:
+                    if count % args.n == 0:
+                        draw_color_histogram(equalized_image if args.equalized else blurred_spinner,colors=['c','y','m'],color_labels=['Hue','Saturation','Value'])
+                    count += 1
 
                 # General Thresholds
                 _, hue_threshold_norm = cv.threshold(individual_channels[0],0,255,cv.THRESH_BINARY|cv.THRESH_OTSU)
                 _, saturation_threshold_inv = cv.threshold(individual_channels[1],0,255,cv.THRESH_BINARY_INV|cv.THRESH_OTSU)
                 _, value_threshold_inv = cv.threshold(individual_channels[2],0,255,cv.THRESH_BINARY_INV|cv.THRESH_OTSU)
 
-                # cv.imshow("hue_threshold_norm", hue_threshold_norm)
-                # cv.imshow("saturation_threshold_inv", saturation_threshold_inv)
-                # cv.imshow("value_threshold_inv", value_threshold_inv)
-
                 # red_threshold = hue_threshold_norm & saturation_threshold_inv & value_threshold_inv
                 red_threshold = cv.bitwise_and(hue_threshold_norm, cv.bitwise_and(saturation_threshold_inv, value_threshold_inv))
-                # cv.imshow("red_threshold", red_threshold)
                 red_contours, _ = cv.findContours(red_threshold,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
                 draw_centers_of_largest_contours(img_in,2,red_contours,(0,0,255),3,offset=(x,y))
 
                 # black_threshold = hue_threshold_norm & !saturation_threshold_inv & value_threshold_inv
                 black_threshold = cv.bitwise_and(hue_threshold_norm, cv.bitwise_and(cv.bitwise_not(saturation_threshold_inv), value_threshold_inv))
-                # cv.imshow("black_threshold", black_threshold)
                 black_contours, _ = cv.findContours(black_threshold,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
                 draw_centers_of_largest_contours(img_in,2,black_contours,(0,0,0),3,offset=(x,y))
 
                 # green_threshold = !hue_threshold_norm & !saturation_threshold_inv & !value_threshold_inv
                 green_threshold = cv.bitwise_and(cv.bitwise_not(hue_threshold_norm), cv.bitwise_and(cv.bitwise_not(saturation_threshold_inv), cv.bitwise_not(value_threshold_inv)))
-                kernel = np.ones((7,7),np.uint8)
+                kernel = np.ones((4,4),np.uint8)
                 green_threshold = cv.morphologyEx(green_threshold,cv.MORPH_OPEN,kernel)
                 cv.imshow("green_threshold", green_threshold)
                 green_contours, _ = cv.findContours(green_threshold,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
                 draw_centers_of_largest_contours(img_in,2,green_contours,(0,255,0),3,offset=(x,y), f=heuristic)
-
-                # add confidence score (heuristics)
 
                 # Showing Spinner Bounding Box
                 cv.rectangle(img_in,(x,y),(x+w,y+h),(0,255,0),2)
