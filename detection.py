11ed7b59f2b5b4986fb36a94d7bb0cb61503bc60
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2020-10-21 12:49:03 -0700

    FEATURE: Added Histograms that Update Across the Video
    This commit makes it so that any histograms displayed will update on one window, and it doesn't block the vision processing.

--- detection.py
+++ detection.py
@@ -6,6 +6,11 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
+created_histogram = False
+figure = None
+axis = None
+individual_histograms = []
+
 def rescale_frame(frame, percent=75):
     width = int(frame.shape[1] * percent/ 100)
     height = int(frame.shape[0] * percent/ 100)
@@ -25,16 +30,24 @@ def draw_centers_of_largest_contours(img, n, contours, color, size, offset=(0,0)
 
 def draw_color_histogram(img, colors=['r','g','b'], color_labels=['Red','Green','Blue']):
     assert len(colors) == len(color_labels), 'Each color does not have a corresponding color_label.'
-    plt.figure()
-    legend_keys = []
-    for i in range(len(colors)):
-        legend_keys.append(mpatches.Patch(color=colors[i], label=color_labels[i]))
-    plt.legend(handles=legend_keys)
-    for i,col in enumerate(colors):
-        histr = cv.calcHist([img],[i],None,[256],[0,256])
-        plt.plot(histr,color = col)
-        plt.xlim([0,256])
-    plt.show()
+    global figure, axis, created_histogram, individual_histograms
+    if not created_histogram:
+        figure, axis = plt.subplots()
+        legend_keys = []
+        for i in range(len(colors)):
+            legend_keys.append(mpatches.Patch(color=colors[i], label=color_labels[i]))
+            component_data, = axis.plot(np.arange(256), np.zeros((256,)), c=colors[i], label=color_labels[i])
+            individual_histograms.append(component_data)
+        plt.legend(handles=legend_keys)
+        axis.set_xlim([0,256])
+        axis.set_ylim([0,5000])
+        plt.show(block=False)
+        created_histogram = True
+    else :
+        for i in range(len(colors)):
+            histr = cv.calcHist([img],[i],None,[256],[0,256])
+            individual_histograms[i].set_ydata(histr)
+            figure.canvas.draw()
 
 def heuristic(contour):
     rect = cv.minAreaRect(contour)
@@ -79,7 +92,8 @@ while cap.isOpened():
 
                 hsv_spinner = cv.cvtColor(spinner,cv.COLOR_BGR2HSV)
                 blurred_spinner = cv.bilateralFilter(hsv_spinner,7,50,50)
-                cv.imshow('blurred_spinner', cv.cvtColor(blurred_spinner,cv.COLOR_HSV2BGR))
+                bgr_blurred_spinner = cv.cvtColor(blurred_spinner,cv.COLOR_HSV2BGR)
+                # cv.imshow('blurred_spinner', bgr_blurred_spinner)
 
                 # Index 0: Hue, Index 1: Saturation, Index 2: Value
                 individual_channels = cv.split(blurred_spinner)
@@ -89,7 +103,24 @@ while cap.isOpened():
                 individual_channels[2] = cv.equalizeHist(individual_channels[2])
 
                 equalized_image = cv.merge((individual_channels[0],individual_channels[1],individual_channels[2]))
-                cv.imshow('equalized_image', cv.cvtColor(equalized_image,cv.COLOR_HSV2BGR))
+                # cv.imshow('equalized_image', cv.cvtColor(equalized_image,cv.COLOR_HSV2BGR))
+
+                # Background Green Detection
+                Lab_spinner = cv.cvtColor(bgr_blurred_spinner, cv.COLOR_BGR2LAB)
+                Lab_individual_channels = cv.split(Lab_spinner)
+                _ , L_threshold = cv.threshold(Lab_individual_channels[0],1,255,cv.THRESH_BINARY|cv.THRESH_OTSU)
+                _ , a_threshold = cv.threshold(Lab_individual_channels[1],1,255,cv.THRESH_BINARY_INV|cv.THRESH_OTSU)
+                b_threshold_value, b_threshold_background = cv.threshold(Lab_individual_channels[2],1,255,cv.THRESH_BINARY|cv.THRESH_OTSU)
+                b_bias = 3
+                b_threshold_background_value = b_bias + b_threshold_value
+                _, b_threshold_background = cv.threshold(Lab_individual_channels[2],b_threshold_background_value,255,cv.THRESH_BINARY)
+                cv.imshow('b_threshold_background',b_threshold_background)
+
+
+                b_edges_bias = 5
+                b_threshold_edges = b_threshold_value - b_edges_bias
+                _, b_threshold_edges = cv.threshold(Lab_individual_channels[2],b_threshold_edges,255,cv.THRESH_BINARY_INV)
+                cv.imshow('b_threshold_edges',b_threshold_edges)
 
                 if args.hist:
                     if count % args.n == 0:
@@ -111,8 +142,10 @@ while cap.isOpened():
                 black_contours, _ = cv.findContours(black_threshold,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
                 draw_centers_of_largest_contours(img_in,2,black_contours,(0,0,0),3,offset=(x,y))
 
-                # green_threshold = !hue_threshold_norm & !saturation_threshold_inv & !value_threshold_inv
-                green_threshold = cv.bitwise_and(cv.bitwise_not(hue_threshold_norm), cv.bitwise_and(cv.bitwise_not(saturation_threshold_inv), cv.bitwise_not(value_threshold_inv)))
+                # green_threshold = !hue_threshold_norm & !saturation_threshold_inv & !value_threshold_inv & !b_threshold_background & !b_threshold_edges
+                green_threshold = cv.bitwise_and(cv.bitwise_and(
+                    cv.bitwise_and(cv.bitwise_not(hue_threshold_norm), cv.bitwise_and(cv.bitwise_not(saturation_threshold_inv), cv.bitwise_not(value_threshold_inv)))
+                    ,cv.bitwise_not(b_threshold_background)),cv.bitwise_not(b_threshold_edges))
                 kernel = np.ones((4,4),np.uint8)
                 green_threshold = cv.morphologyEx(green_threshold,cv.MORPH_OPEN,kernel)
                 cv.imshow("green_threshold", green_threshold)
